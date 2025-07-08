ebcc7c8bc50ca20b47e9c8bbfa2b753185a0c8ee
varunmarulkar47 <varunmarulkar47@gmail.com>
2025-07-08 15:11:10 +0530

    initial change

--- src/Components/AddBook.jsx
+++ src/Components/AddBook.jsx
@@ -16,14 +16,14 @@ const AddBook = () => {
         isbn: "",
     })
 
-    const navigate=useNavigate()
+    const navigate = useNavigate()
 
     const dispatch = useDispatch()
 
-    function handleSubmit(e){
+    function handleSubmit(e) {
         e.preventDefault()
-        const isEmpty=Object.values(formData).some(val=>val.trim()==="")
-        if(isEmpty){
+        const isEmpty = Object.values(formData).some(val => val.trim() === "")
+        if (isEmpty) {
             alert("Please Fill Required Fields")
             return;
         }
@@ -34,36 +34,36 @@ const AddBook = () => {
     }
     return (
         <div className='h-screen bg-gray-300 border'>
-     <Link to="/"> <h1 className='m-2 hover:text-red-400 hover:underline '>⬅️Back To Home Page</h1></Link>  
-        <div className='flex flex-col items-center justify-center '>  
-        <h1 className='font-black text-4xl'>Add Book</h1>
-            <form className='flex flex-col gap-4 m-5'> 
-                    
-                <input className='border w-50  rounded-sm text-center' type="text" name="id" placeholder='Id' value={formData.id}
-                    onChange={(e) => setFormData({ ...formData, id: e.target.value })} />
+            <Link to="/"> <h1 className='m-2 hover:text-red-400 hover:underline '>⬅️Back To Home Page</h1></Link>
+            <div className='flex flex-col items-center justify-center '>
+                <h1 className='font-black text-4xl'>Add Book</h1>
+                <form className='flex flex-col gap-4 m-5'>
 
-                <input className='border w-50  rounded-sm text-center'  type="text" name="title" placeholder='Title' value={formData.title}
-                    onChange={(e) => setFormData({ ...formData, title: e.target.value })} />
+                    <input className='border w-50  rounded-sm text-center' type="text" name="id" placeholder='Id' value={formData.id}
+                        onChange={(e) => setFormData({ ...formData, id: e.target.value })} />
+
+                    <input className='border w-50  rounded-sm text-center' type="text" name="title" placeholder='Title' value={formData.title}
+                        onChange={(e) => setFormData({ ...formData, title: e.target.value })} />
 
 
-                <input className='border w-50  rounded-sm text-center'  type="text" name="author" placeholder='Author' value={formData.author}
-                    onChange={(e) => setFormData({ ...formData, author: e.target.value })} />
+                    <input className='border w-50  rounded-sm text-center' type="text" name="author" placeholder='Author' value={formData.author}
+                        onChange={(e) => setFormData({ ...formData, author: e.target.value })} />
 
-                <input className='border w-50  rounded-sm text-center'  type="text" name="category" placeholder='Category' value={formData.category}
-                    onChange={(e) => setFormData({ ...formData, category: e.target.value })} />
+                    <input className='border w-50  rounded-sm text-center' type="text" name="category" placeholder='Category' value={formData.category}
+                        onChange={(e) => setFormData({ ...formData, category: e.target.value })} />
 
-                <input className='border w-50  rounded-sm text-center'  type="text" name="description" placeholder='Description' value={formData.description}
-                    onChange={(e) => setFormData({ ...formData, description: e.target.value })} />
+                    <input className='border w-50  rounded-sm text-center' type="text" name="description" placeholder='Description' value={formData.description}
+                        onChange={(e) => setFormData({ ...formData, description: e.target.value })} />
 
-                <input className='border w-50  rounded-sm text-center'  type="number" name="rating" placeholder='Rating' value={formData.rating}
-                    onChange={(e) => setFormData({ ...formData, rating: e.target.value })} />
+                    <input className='border w-50  rounded-sm text-center' type="number" name="rating" placeholder='Rating' value={formData.rating}
+                        onChange={(e) => setFormData({ ...formData, rating: e.target.value })} />
 
-                <input className='border w-50  rounded-sm text-center'  type="text" name="isbn" placeholder='ISBN' value={formData.isbn}
-                    onChange={(e) => setFormData({ ...formData, isbn: e.target.value })} />
+                    <input className='border w-50  rounded-sm text-center' type="text" name="isbn" placeholder='ISBN' value={formData.isbn}
+                        onChange={(e) => setFormData({ ...formData, isbn: e.target.value })} />
 
-                <button className='border bg-red-500 rounded-2xl text-white cursor-pointer' type='submit' onClick={handleSubmit}>Submit</button>
-            </form>
-        </div>
+                    <button className=' border text-black hover:bg-red-500 rounded-2xl cursor-pointer p-1' type='submit' onClick={handleSubmit}>Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
